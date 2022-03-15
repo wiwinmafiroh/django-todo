ccbacfa147a3ec3c4f1fccf54ff0e1ef2760e665
Wiwin Mafiroh <wiwinmafiroh@gmail.com>
2022-03-15 20:09:50 +0700

    Update settings

--- website/settings.py
+++ website/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-c!-a#79t9z7vrw-$gh_x-7_)^l%%8=c492swm6z0%+nlgo205_'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-c!-a#79t9z7vrw-$gh_x-7_)^l%%8=c492swm6z0%+nlgo205_')
+SECRET_KEY = 'django-insecure-c!-a#79t9z7vrw-$gh_x-7_)^l%%8=c492swm6z0%+nlgo205_'
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-c!-a#79t9z7vrw-$gh_x-7_)^l%%8=c492swm6z0%+nlgo205_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['wiwinmafiroh-django-todo.herokuapp.com', '127.0.0.1']
 
@@ -85,8 +85,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
