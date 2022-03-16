70bb6d4c442d476ee951791c091be4c7b6addf7d
Wiwin Mafiroh <wiwinmafiroh@gmail.com>
2022-03-16 13:25:29 +0700

    Changes required for deployment to heroku

--- website/settings.py
+++ website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-c!-a#79t9z7vrw-$gh_x-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') != 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # Application definition
 
